34c5f5fa0efe1e10a0fbb878c8880d4fa2be6f56
EndzyCodes <127296867+ReginioMarkGilbert@users.noreply.github.com>
2024-05-20 12:46:50 +0800

    change api endpoints

--- src/components/ApplicationForm.js
+++ src/components/ApplicationForm.js
@@ -24,7 +24,7 @@ const ApplicationForm = ({ onSubmit }) => {
         };
 
         try {
-            const response = await fetch('http://localhost:5000/api/applications', {
+            const response = await fetch('https://permittree-api.netlify.app/api/applications', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
